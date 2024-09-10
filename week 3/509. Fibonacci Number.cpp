e5c665dea89bbb1d920f7fb2912cf07f3cbb5970
Sujal <Sujalverma32433@gmail.com>
2024-09-10 21:01:11 +0530

    Create 509. Fibonacci Number.cpp

---
+++ week 3/509. Fibonacci Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0,b=1;
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        else{
+            for(int i=2;i<n;i++)
+            {
+                int c=a+b;
+                a=b;
+                b=c;
+            }
+        }
+        return (a+b);
+    }
+};
