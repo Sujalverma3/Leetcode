abb3f3717804bb33035cf41718dc36e54367b492
Sujal <Sujalverma32433@gmail.com>
2024-10-22 19:34:27 +0530

    Update 2807. Insert Greatest Common Divisors in Linked List.cpp

--- week 8/2807. Insert Greatest Common Divisors in Linked List.cpp
+++ week 8/2807. Insert Greatest Common Divisors in Linked List.cpp
@@ -1,4 +1,5 @@
-/**
+
+   /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -10,29 +11,40 @@
  */
 class Solution {
 public:
-    int Gcd(int n1, int n2) {
-        int divisor = 0;
-        for (int i = 1; i <= n1 && i <= n2; i++) {
-            if (n1 % i == 0 && n2 % i == 0) {
-                divisor = i;
-            }
-        }
-        return divisor;
-    }
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         if (head == nullptr && head->next == nullptr) {
             return head;
         }
         ListNode *curr = head, *curnext = head->next;
-        while (curr != nullptr && curnext != nullptr) {
+        while (curnext != nullptr) {
             ListNode* temp =new ListNode(Gcd(curr->val, curnext->val),curnext);
             curr->next = temp;
             curr = curnext;
-            if (curr!=nullptr || curr->next != nullptr)
-                curnext = curnext->next;
-            else
-                break;
+            curnext = curnext->next;
         }
         return head;
+    }
+    private:
+    // int Gcd(int a, int b) {
+    //     while (b != 0) {
+    //         int temp = b;
+    //         b = a % b;
+    //         a = temp;
+    //     }
+    //     return a;
+    // }
+    
+    // int Gcd(int n1, int n2) {
+    //     int divisor = 0;
+    //     for (int i = 1; i <= n1 && i <= n2; i++) {
+    //         if (n1 % i == 0 && n2 % i == 0) {
+    //             divisor = i;
+    //         }
+    //     }
+    //     return divisor;
+    // }
+    int Gcd(int a, int b){
+        if(a == 0) return b;
+        else return gcd(b%a, a);
     }
 };
