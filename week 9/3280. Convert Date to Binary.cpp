0967ec0733cfb3a01b3b2248d6e81591a1566f5c
Sujal <Sujalverma32433@gmail.com>
2024-11-01 21:19:37 +0530

    Create 3280. Convert Date to Binary.cpp

---
+++ week 9/3280. Convert Date to Binary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    string convert(int num)
+    {
+        string s;
+        while(num)
+        {
+            s+= to_string(num%2);
+            num /= 2;
+        }
+        reverse(s.begin(), s.end());
+
+        return s;
+    }
+public:
+    string convertDateToBinary(string date) {
+        string ans;
+        string temp;
+        for(auto c: date)
+        {
+            if(c == '-')
+            {
+                if(!ans.empty()) ans += '-';
+                ans += convert(stoi(temp));
+                temp = "";
+            }
+            else
+                temp += c;
+        }
+        ans += '-';
+        ans += convert(stoi(temp));
+        return ans;
+    }
+};
